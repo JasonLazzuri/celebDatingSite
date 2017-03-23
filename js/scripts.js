0400e5df497b97e3272c4ffaea0815874bf117fe
Dan Jacob and jason Lazzuri <jason@>
2017-03-23 14:35:05 -0700

    added styling

--- js/scripts.js
+++ js/scripts.js
@@ -5,18 +5,31 @@ $(document).ready(function() {
     var color = $("#color").val();
     var setting = $("#setting").val();
 
-    if (gender === "male" && color === "red" && (setting === "mountain"||"city")){
+    if (gender === "male" && color === "red" && setting === "mountain"||"city"){
       $(".tina").show();
-    } else if (gender === "male" && color === "blue" && (setting === "mountain"||"city")){
-        $(".amy").show();
-    }
-    if (gender === "female" && color === "red" && (setting === "mountain"||"city")){
-      $(".ray").show();
-    } else if (gender === "female" && color === "blue" && (setting === "mountain"||"city")){
-        $(".adam").show();
-    }
+    } else if (!gender, color === "red" && setting === "mountain"||"city"); {
+      alert('Please enter in the field');
+}
+
+    // }else if (gender === "male" && color === "blue" && (setting === "mountain"||"city")){
+    //     $(".amy").show();
+    // }
+    // if (gender === "female" && color === "red" && (setting === "mountain"||"city")){
+    //   $(".ray").show();
+    // } else if (gender === "female" && color === "blue" && (setting === "mountain"||"city")){
+    //     $(".adam ").show();
+    // }
+    // if (!gender, !color, !setting);
+    //   alert('Please enter in the field');
+    //
 
     event.preventDefault()
 
   });
 });
+// if ((gender != "male"||"female")||(color != "blue" || "red")||(setting != "city"|| "mountain"));
+// alert("please enter the correct input"),
+// else{
+//   if ((gender != "male"||"female")||(color != "blue" || "red")||(setting != "city"|| "mountain"));
+//   alert("please enter the correct input"),
+// }
