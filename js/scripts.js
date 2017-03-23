c04cef3672f0ab8a694974fd19830f2826780712
Dan Jacob and jason Lazzuri <dan@>
2017-03-23 13:23:37 -0700

    Used parentheses for and or to run smoothly

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,22 @@
-$(document).ready(function(){
+$(document).ready(function() {
   $("#form").submit(function(event) {
 
+    var gender = $("#gender").val();
+    var color = $("#color").val();
+    var setting = $("#setting").val();
+
+    if (gender === "male" && color === "red" && (setting === "mountain"||"city")){
+      $(".tina").show();
+    } else if (gender === "male" && color === "blue" && (setting === "mountain"||"city")){
+        $(".amy").show();
+    }
+    if (gender === "female" && color === "red" && (setting === "mountain"||"city")){
+      $(".ray").show();
+    } else if (gender === "female" && color === "blue" && (setting === "mountain"||"city")){
+        $(".adam").show();
+    }
 
     event.preventDefault()
+
   });
 });
